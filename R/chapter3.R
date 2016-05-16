b258c2eccc1d0beb4c3f20d38c5b2c11c5840fed
FEMKE THON <femke_thon@hotmail.com>
2016-05-16 20:42:53 +0200

    added some to chapter 3

--- R/chapter3.R
+++ R/chapter3.R
@@ -5,10 +5,16 @@
 #' @author Femke Thon
 
 chapter3 <- function(input){
-  file          <- NULL
+  file                <- NULL
   set.seed(1410)
-  file$data           <- ggplot2::diamonds[
-                                  sample(nrow(ggplot2::diamonds), 100), ]
+  file$data           <- input
+  file$plots$scatter  <- qplot(displ, hwy, data = file$data,
+                               colour = factor(cyl))
+  file$plots$layered  <- qplot(displ, hwy, data = file$data, facets = .~year) +
+                               geom_smooth()
+  file$plots$laycol   <- qplot(displ, hwy, data = file$data, facets = .~cyl,
+                               color = factor(cyl)) + geom_smooth()
+
   file
 }
 
